7a21a6d1c5c6e6ab9a9c292d9c721ecc689e2ecb
Call Vin <kanggara75@gmail.com>
2024-03-23 09:41:41 +0700

    add: NavBar

---
+++ resources/views/components/navbar/link.blade.php
@@ -0,0 +1,4 @@
+<a {{ $attributes }}
+  class="{{ request()->is($href) ? 'bg-gray-700 text-white' : 'text-gray-300 hover:bg-gray-800 hover:text-white' }} rounded-md px-3 py-2 text-sm">
+  {{ $slot }}
+</a>
